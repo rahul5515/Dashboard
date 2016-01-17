f598fcf41570ee03e63b745941c55270601f8177
rahul5515 <rahul.5515@gmail.com>
2016-01-17 22:58:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
 var App = angular.module('App', []);
 
- App.config(['$routeProvider', function($routeProvider)
- {  
-  $routeProvider.     
-when('/Add', 
-{	
+App.config(['$routeProvider', function($routeProvider)
+{  
+	$routeProvider.     
+	when('/Add', 
+	{	
 templateUrl: 'Add.html',	
 controller: 'AddController'
-      }).     
-when('/Show', {
-	templateUrl: 'Dashboard.html',	
+	}).     
+	when('/Show', {
+templateUrl: 'Dashboard.html',	
 controller: 'ShowOrdersController'
-      }).     
-when('/Dbhk', {
-	templateUrl: 'DashboardHK.html',	
+	}).     
+	when('/Dbhk', {
+templateUrl: 'DashboardHK.html',	
 controller: 'DashboardHKController'
-      }).     
-when('/ShowDetail/:date', {
-	templateUrl: 'Details.html',	
+	}).     
+	when('/ShowDetail/:date', {
+templateUrl: 'Details.html',	
 controller: 'DetailsController'
-      })
-.otherwise({
-        redirectTo: '/Dbhk'
-      });
+	})
+	.otherwise({
+redirectTo: '/Dbhk'
+	});
 }]);
 
+App.directive("tempDirective", function()
+{
+	return{template:"<select><option id='option1' value='Green'>Green</option><option id='option2' value='Amber'>Amber</option><option id='option3' value='Red'>Red</option></select>"};
+});
+
 App.controller('TodoCtrl', function($scope, $http, $location) {
-  $http.get('todos.json')
-       .then(function(res){
-          $scope.todos = res.data;             
-        });		
-		 $scope.IsNav = $location.path() != '/Dbhk';		
+	$http.get('todos.json')
+	.then(function(res){
+		$scope.todos = res.data;             
+	});		
+	$scope.IsNav = $location.path() != '/Dbhk';
+	$scope.options =  [{name:'Green', color:'option1'},{name:'Amber', color:'option2'},{name:'Red', color:'option3'}];
+	$scope.myselect = 'Green';
 });
 
 App.controller('DetailsController', function($scope, $http, $routeParams) {
@@ -39,13 +46,13 @@ App.controller('DetailsController', function($scope, $http, $routeParams) {
 
 App.controller('AddController', function($scope) {	
 	$scope.name = "Rahul";
- $scope.date = new Date(); 
+	$scope.date = new Date(); 
 
 });
 
 App.controller('DashboardHKController', function($scope, $http) {
-$http.get('todos.json')
-       .then(function(res){
-          $scope.todos = res.data;            
-        });
+	$http.get('todos.json')
+	.then(function(res){
+		$scope.todos1 = res.data;            
+	});
 });
